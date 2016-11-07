7ec2fc17abe5dc438520df513eae2b6c2bd64a3b
Topi99 <nokoayzack@gmail.com>
2016-11-06 20:21:19 -0600

    Listo para subit

---
+++ main.js
@@ -0,0 +1,12 @@
+import("jquery-3.1.0.min.js")
+
+var good = "<span class='good'>¡Bien!</span>";
+var wrong = "<span class='wrong'>¡Mal!</span>"
+
+var answers = ["direcciones IP", "nombre de domino", "DNS"]
+
+$("#done").on("click", function(){
+	
+	alert("Hola k pez");
+	
+});
